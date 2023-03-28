dd8fbb35086e816d085e6b3422882c34502dc836
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-03-28 17:27:47 -0300

    feat: create load more content button

---
+++ game_tracker/src/components/LoadButton/LoadButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="load-content">
+        <button class="load-btn">
+            <span>{{ more_content }}</span>
+        </button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "LoadButton",
+    data() {
+        return {
+            more_content: "Carregar mais"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/components/_load-button.scss";
+</style>
